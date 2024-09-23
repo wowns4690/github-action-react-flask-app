9357b012d531ff61cccbd49cb2ac1e8caac478f6
eojiinn <djwls2726@gmail.com>
2024-09-23 12:35:50 +0900

    add

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ CORS(app)
 
 @app.route('/api/hello', methods=['GET'])
 def hello():
-    return jsonify(message="Hello, World!")
+    return jsonify(message="Hello, World!!")
 
 if __name__ == '__main__':
     app.run(debug=True)
