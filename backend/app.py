5a92cd66f0f5502e68d8ce34ab353e4f9ec8c684
hanjaejun <wowns4690@kakao.com>
2024-09-23 13:57:02 +0900

    DB connect and get table items

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,33 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+import boto3
+from botocore.exceptions import ClientError
 
 app = Flask(__name__)
 CORS(app)
+
+# AWS DynamoDB 클라이언트 설정
+dynamodb = boto3.resource(
+    'dynamodb',
+    region_name='ap-northeast-2',  # AWS 리전 설정
+    aws_access_key_id='{{ AWS_ACCESS_KEY_ID }}',  # GitHub Actions에서 환경 변수로 설정
+    aws_secret_access_key='{{ AWS_SECRET_ACCESS_KEY }}'  # GitHub Actions에서 환경 변수로 설정
+)
+
+# DynamoDB 테이블 설정
+table = dynamodb.Table('crud-table')
+
+#DB crud-table 항목 가져오기
+@app.route('/api/list', methods=['GET'])
+def get_list():
+    try:
+        response = table.scan()
+        items = response.get('Items', [])
+        return jsonify(items), 200
+    except ClientError as e:
+        return jsonify({"error":str(e)}), 500
+
+
 
 @app.route('/api/hello', methods=['GET'])
 def hello():
