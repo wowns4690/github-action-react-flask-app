f246a925c4e15c2a57226a7c513ce16c5a5b1383
eojiinn <djwls2726@gmail.com>
2024-09-23 14:44:48 +0900

    feat: POST 추가

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import boto3
 from botocore.exceptions import ClientError
@@ -16,6 +16,15 @@ dynamodb = boto3.resource(
 # DynamoDB 테이블 설정
 table = dynamodb.Table('crud-table')
 
+def get_next_id():
+    response = table.scan()
+    if 'Items' in response and len(response['Items']) > 0:
+        ids = max([item['id'] for item in response['Items']])
+        return ids + 1
+    return 1
+
+next_id = get_next_id()
+
 #DB crud-table 항목 가져오기
 @app.route('/api/list', methods=['GET'])
 def get_list():
@@ -26,7 +35,23 @@ def get_list():
     except ClientError as e:
         return jsonify({"error":str(e)}), 500
 
+@app.route("/api/diaries", methods=['POST'])
+def create_diary():
+    global next_id
+    data = request.json
 
+    if 'title' not in data or 'content' not in data:
+        return jsonify({'error': 'Title and content are required'}), 400
+    
+    diary = {
+        'id': next_id,
+        'title': data['title'],
+        'content': data['content']
+    }
+
+    table.put_item(Item=diary)
+    next_id += 1
+    return jsonify(diary), 201
 
 @app.route('/api/hello', methods=['GET'])
 def hello():
